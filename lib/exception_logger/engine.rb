f1e7598309fdc184a2fdabdf9bae61c6325efc37
Alex Farrill <alex.farrill@gmail.com>
2012-05-18 08:44:11 -0400

    add assets to precompile list

--- lib/exception_logger/engine.rb
+++ lib/exception_logger/engine.rb
@@ -5,5 +5,10 @@ require "i18n"
 
 module ExceptionLogger
   class Engine < Rails::Engine
+    if Rails.version > "3.1"
+      initializer "ExceptionLogger precompile hook" do |app|
+        app.config.assets.precompile += ['exception_logger.js', 'exception_logger.css']
+      end
+    end
   end
 end
